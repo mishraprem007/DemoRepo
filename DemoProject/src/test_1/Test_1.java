bed4b41a01b7323ff507fd9bef3f0aed4731ae2a
mishrapremshankar1995 <mishrapremshankar1995@gmail.com>
2026-03-30 23:05:26 +0530

    removed print statement added bu u1

--- DemoProject/src/test_1/Test_1.java
+++ DemoProject/src/test_1/Test_1.java
@@ -4,6 +4,7 @@ public class Test_1 {
 	public static void main(String[] args) {
 
 		System.out.println("Test_1>>1- initails commit");
-		System.out.println("Added by U1");
+		//removed print statement added bu u1
+		
 	}
 }

9569d2cdf61ab692ddb5cff4e8c8482a59adbc2f
mishraprem007 <mishraprem007@gmail.com>
2026-03-30 23:19:55 +0530

    fremoved junked code

--- DemoProject/src/test_1/Test_1.java
+++ DemoProject/src/test_1/Test_1.java
@@ -3,12 +3,7 @@ package test_1;
 public class Test_1 {
 	public static void main(String[] args) {
 
-		System.out.println("Test_1>>1- initails commit");
-
-		System.out.println("Added by U1");
-
-		System.out.println("Expecting git conflict");
-		// removed print statement added bu u1
+		System.out.println("Fresh Code");
 
 	}
 }
